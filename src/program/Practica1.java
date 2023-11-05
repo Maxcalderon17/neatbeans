049e495449108960e4b167cde5115896b3ded155
maxim <maxim@LAPTOP-IAF8CTJT>
2023-11-04 23:43:39 -0500

    updated

---
+++ src/program/Practica1.java
@@ -0,0 +1,10 @@
+/*Maximus Ocampo Calderon Tics practica1 mensaje en pantalla*/
+package program;
+
+
+public class Practica1 {
+
+    public static void main(String[] args) {
+        System.out.println("Este es un mensaje en pantalla");
+    }
+}
